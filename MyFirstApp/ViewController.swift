5562ce610a3387c6266def3d4129d08161d4f0e8
Pedro Antonio Vazquez Rodriguez <pedrovazquezax@users.noreply.github.com>
2017-08-31 18:45:17 -0500

    primercomit
    primer

--- MyFirstApp/ViewController.swift
+++ MyFirstApp/ViewController.swift
@@ -9,17 +9,63 @@
 import UIKit
 
 class ViewController: UIViewController {
+ 
+    @IBOutlet weak var lblscore: UILabel!
+    @IBOutlet weak var lblcambia: UILabel!
+    
 
+    
+    
+    var randomNumber = -1
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        generalColor()
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    var colors=[UIColor.red,UIColor.green,UIColor.blue,UIColor.purple]
+    var colorsText=["Rojo","Verde","Azul","Morado"]
+    var score = 0
+    
+  
+    func generalColor(){
+        randomNumber = Int(arc4random_uniform(UInt32(colorsText.count)))
+        lblcambia.text = colorsText[randomNumber]
+        
+        let randomColor = Int(arc4random_uniform(UInt32(colorsText.count)))
+        lblcambia.textColor = colors[randomColor]
+    
     }
-
+    
+   
+    
+    
+    @IBAction func btnpresionado(_ sender: UIButton) {
+        
+        if randomNumber == sender.tag{
+            score += 1}
+        else{
+            score -= 3
+        }
+        lblscore.text = "Score: \(score)"
+        generalColor()
+        
+    }
+    
+   @IBAction func btnstart(_ sender: UIButton) {
+    
+    
+        lblscore.text = "Score: 0"
+    
+  
+        
+    }
+     /*
+     func scheduledTimer(timeInterval: TimeInterval, invocation: NSInvocation, repeats: True){
+     }
+     */
+    
+    
 
 }
 
